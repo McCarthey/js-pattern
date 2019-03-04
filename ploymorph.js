c6555e38437bddb38e4dd2c6d0e45a5006b4ca03
mccarthey <mccarthey123@outlook.com>
2019-03-04 22:12:07 +0800

    make sound by Prototype

--- ploymorph.js
+++ ploymorph.js
@@ -1,13 +1,16 @@
 var makeSound = function(animal) {
-  if(animal instanceof Duck) {
-    console.log('Gagaga')
-  } else if(animal instanceof Chicken) {
-    console.log('Gegege')
-  }
+  animal.sound()
 }
 
 var Duck = function() {}
+Duck.prototype.sound = function() {
+  console.log('Gagaga')
+}
+
 var Chicken = function() {}
+Chicken.prototype.sound = function() {
+  console.log('Gegege');
+}
 
 makeSound(new Duck()) // 'Gagaga'
 makeSound(new Chicken()) // 'Gegege'
