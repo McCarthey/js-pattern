3a91074768355e168a1f8b22da69061e7ae80f1c
mccarthey <mccarthey123@outlook.com>
2019-03-11 23:06:28 +0800

    call apply

--- call-apply.js
+++ call-apply.js
@@ -11,3 +11,26 @@ var func = function (a, b, c) {
 }
 
 func.apply(null, [1, 2, 3])
+
+Math
+  .max
+  .apply(null, [1, 2, 5, 4, 3])
+
+
+var obj1 = {
+  name: 'sven'
+}
+
+var obj2 = {
+  name: 'anne'
+}
+
+window.name ='window'
+
+var getName = function() {
+  console.log(this.name)
+}
+
+getName() // 'window'
+getName.apply(obj1) // 'sven'
+getName.call(obj2) // 'anne'
